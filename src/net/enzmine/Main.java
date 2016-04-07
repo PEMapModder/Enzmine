e93a86d6c59fe8edf7f76ae92a527f5a4f68c49e
PEMapModder <pemapmodder1970@gmail.com>
2016-04-07 20:23:35 +0800

    Initial commit

---
+++ src/net/enzmine/Main.java
@@ -0,0 +1,24 @@
+package net.enzmine;
+
+/*
+ * This file is part of Enzmine.
+ *
+ * Enzmine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Enzmine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Enzmine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+public class Main{
+	public static void main(String[] args){
+
+	}
+}
